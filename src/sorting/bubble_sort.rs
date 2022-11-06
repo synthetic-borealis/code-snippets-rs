1580cef1161f926b8d214ac02ebc8ee10ce0eef5
synthetic-borealis <snip.coloured@gmail.com>
2022-11-06 20:58:47 +0200

    bubble sort

---
+++ src/sorting/bubble_sort.rs
@@ -0,0 +1,29 @@
+pub fn bubble_sort<T>(v: &mut Vec<T>) where T: Ord + Copy {
+    loop {
+        let mut swaps = 0;
+
+        for i in 0..v.len() - 1 {
+            if v[i] > v[i + 1] {
+                v.swap(i, i + 1);
+                swaps += 1;
+            }
+        }
+
+        if swaps == 0 {
+            break;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bubble_sort() {
+        let mut test_vector = vec![5, 1, 8, -12, 22, 9];
+        let expected = vec![-12, 1, 5, 8, 9, 22];
+        bubble_sort(&mut test_vector);
+        assert_eq!(test_vector, expected);
+    }
+}
